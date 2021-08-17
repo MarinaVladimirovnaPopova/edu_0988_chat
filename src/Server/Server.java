79457aa45969d8f5dd3832629c4fcd74a84b1e8b
MarinaVladimirovnaPopova <88132239+MarinaVladimirovnaPopova@users.noreply.github.com>
2021-08-18 00:41:31 +0300

    Update Server.java

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,4 +1,4 @@
-package server;
+package Server;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -6,14 +6,13 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
-import java.util.Locale;
 
 public class Server {
     public static void main(String[] args) {
         {
             ArrayList<User> users = new ArrayList<>();
             try {
-                ServerSocket serverSocket = new ServerSocket(8168); // Создаём серверный сокет
+                ServerSocket serverSocket = new ServerSocket(8188); // Создаём серверный сокет
                 System.out.println("Сервер запущен");
                 while (true) { // Бесконечный цикл для ожидания родключения клиентов
                     Socket socket = serverSocket.accept(); // Ожидаем подключения клиента
@@ -29,41 +28,29 @@ public class Server {
                                 out.writeUTF("Добро пожаловать на сервер");
                                 out.writeUTF("Введите ваше имя: ");
                                 String userName = in.readUTF(); // Ожидаем имя от клиента
-                                while (FreeName(users, userName)) { // проверка имени
-                                    System.out.println("клиент ввел имя, используемое другим клиентом");
-                                    out.writeUTF("Это имя - " + userName + " - уже занято, выберете другое имя.");
-                                    userName = in.readUTF();
 
+                                while (checkFreeName(users, userName)) { // Проверяем, свободно ли имя
+                                    out.writeUTF("Введенное имя: " + userName + " используется другим пользователем.Подумайте ещё...");
+                                    userName = in.readUTF();
                                 }
                                 currentUser.setUserName(userName);
-                                System.out.println(currentUser.getUserName() + " присоединяется к беседе");
 
                                 for (User user : users) {
                                     DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
-                                    out.writeUTF(currentUser.getUserName() + " присоединяется к беседе");
+                                    out.writeUTF(currentUser.getUserName() + " присоединился к беседе");
                                 }
                                 while (true) {
                                     String request = in.readUTF(); // Ждём сообщение от пользователя
                                     System.out.println(currentUser.getUserName() + ": " + request);
-                                    if (request.contains("/m")) {
-                                        privat(request, users, currentUser);
-                                    }
-                                    for (User user : users) {
-                                        if (users.indexOf(user) == users.indexOf(currentUser)) continue;
-                                        DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
-                                        out.writeUTF(currentUser.getUserName() + ": " + request);
+                                    if (request.startsWith("/m ")) {
+                                        String replacedRequest = request.replaceFirst("/m ", "");
+                                        sendPrivateMessage(replacedRequest, users, currentUser);
+                                    } else {
+                                        sendPublicMessage(request, users, currentUser);
                                     }
                                 }
                             } catch (IOException e) {
-                                users.remove(currentUser);
-                                for (User user : users) {
-                                    try {
-                                        DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
-                                        out.writeUTF(currentUser.getUserName() + " покинул(а) чат");
-                                    } catch (IOException ioException) {
-                                        ioException.printStackTrace();
-                                    }
-                                }
+                                userExit(users, currentUser);
                             }
                         }
                     });
@@ -75,35 +62,69 @@ public class Server {
         }
     }
 
-    public static boolean FreeName(ArrayList<User> users, String name) {
+    private static void sendPublicMessage(String request, ArrayList<User> users, User currentUser) throws IOException {
         for (User user : users) {
+            if (users.indexOf(user) == users.indexOf(currentUser)) continue;
+            DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
+            out.writeUTF(currentUser.getUserName() + ": " + request);
+        }
+    }
+
+    private static void sendPrivateMessage(String request, ArrayList<User> users, User currentUser) throws IOException {
+        User recipient = null;
+        String[] splitted = request.split(" ", 2);
+        String name = "";
+        String message = "";
+        DataOutputStream out = new DataOutputStream(currentUser.getSocket().getOutputStream());
+        try {
+            name = splitted[0];
+            message = splitted[1];
+        } catch (IndexOutOfBoundsException ex) {
+            out.writeUTF("Некорректное сообщение");
+        }
+
+        for (User user : users) {
+            if (user.getUserName().equals(name)) {
+                recipient = user;
+                break;
+            }
+        }
+        if (recipient != null) {
+            DataOutputStream recipientOut = new DataOutputStream(recipient.getSocket().getOutputStream());
+            recipientOut.writeUTF(currentUser.getUserName() + ": " + message);
+        } else {
+            out.writeUTF("Пользователь с таким именем: " + name + " не найден");
+        }
+
+
+//        for (User user : users) {
+//            if (users.indexOf(user) == users.indexOf(currentUser)) continue;
+//            DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
+//            out.writeUTF(currentUser.getUserName() + ": " + request);
+//        }
+
+    }
+
+
+    private static void userExit(ArrayList<User> users, User currentUser) {
+        users.remove(currentUser);
+        for (User user : users) {
+            try {
+                DataOutputStream out = new DataOutputStream(user.getSocket().getOutputStream());
+                out.writeUTF(currentUser.getUserName() + " покинул чат");
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+    }
+
+    public static boolean checkFreeName(ArrayList<User> users, String name) {
+        for (User user : users
+        ) {
             if (user.getUserName() != null && user.getUserName().equals(name)) {
                 return true;
             }
         }
         return false;
-    }
-
-    private static void privat(String request, ArrayList<User> users, User currentUser) throws IOException {
-        User mUser = null;
-        String[] split = (request.split(" ", 3));
-        String name = "";
-        DataOutputStream out = new DataOutputStream(currentUser.getSocket().getOutputStream());
-
-            name = split[1];
-        
-
-        for (User user : users) {
-            if (user.getUserName().equals(name)) {
-                mUser = user;
-                break;
-            }
-        }
-        if (mUser != null) {
-            DataOutputStream recipientOut = new DataOutputStream(mUser.getSocket().getOutputStream());
-            recipientOut.writeUTF(currentUser.getUserName() + ": " + request);
-        } else {
-            out.writeUTF("Пользователя с именем " + name + " нет");
-        }
     }
 }
